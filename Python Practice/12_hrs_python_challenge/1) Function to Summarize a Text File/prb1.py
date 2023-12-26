61fe644ed902830d5439924d006636169af40f52
vamsimadhav <vamsimadhav2000@gmail.com>
2023-12-27 00:03:47 +0530

    Solved 1/15 prbs for checking python knowledge

---
+++ Python Practice/12_hrs_python_challenge/1) Function to Summarize a Text File/prb1.py
@@ -0,0 +1,28 @@
+def summarize_text_file(filename):
+
+    with open(filename, 'r') as text_file:
+        # No of Lines
+        content = text_file.readlines()
+        no_of_lines = len(content)
+
+        # No of Words
+        words = []
+        for line in content:
+            words += line.split()
+        no_of_words = len(words)
+
+        # No of Character's
+        no_of_character = 0
+        for word in words:
+            i = 0
+            while i < len(word):
+                no_of_character += 1
+                i += 1
+
+    print("No of lines: " + str(no_of_lines))
+    print("No of words: " + str(no_of_words))
+    print("No of characters: " + str(no_of_character))
+
+
+name = str(input("Enter file name: "))
+summarize_text_file(name)
